0b9505494709e623e3a08ca854d651276fe0ae7a
sunguh0904 <sunguh0904@gmail.com>
2024-08-01 19:57:39 +0900

    4-5 탈출문자

---
+++ practice4-5.py
@@ -0,0 +1,23 @@
+# \n: 줄바꿈
+# print("백문이 불여일견\n백건이 불여일타")
+
+# \", \': 문장 내에서 그대로 해석할 수 있다."
+# print("저는 '홍길동' 입니다.")
+# print('저는 "홍길동" 입니다.')
+# print("저는 \"홍길동\" 입니다.")
+# print("저는 \'홍길동\' 입니다.")
+
+#  \\: 문장 내에서는 하나의 \로 바뀜
+print("C:\\WEB\\dev\\lecture-code\\PythonWorkspace")
+
+# \r: 커서를 맨 앞으로 이동
+""" 
+\r 앞에 문자열 길이만큼(공백 포함) 처음 순수버투 덮어씌우기
+ """
+print("Red Apple\rPine")
+
+#  \b: 백스페이스(한 글자 삭제)
+print("Redd\bApple")
+
+# \t: 탭 역할
+print("Red\tApple")
