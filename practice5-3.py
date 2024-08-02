db56489f7d37cb30670040197a159047fb5fbc34
sunguh0904 <sunguh0904@gmail.com>
2024-08-02 21:48:36 +0900

    5-3 튜플

---
+++ practice5-3.py
@@ -0,0 +1,13 @@
+menu = ("돈까스", "치즈까스")
+print(menu[0])
+print(menu[1])
+
+# menu.add("생선까스")  # 튜플: 한 번 선언 되면 추가하거나 삭제가 불가능
+
+name = "김종국"
+age = 20
+hobby = "코딩"
+print(name, age, hobby)
+
+name, age, hobby = ("김종국", 20, "코딩")   # 서로다른 변수에 서로 다른 값 할당 가능
+print(name, age, hobby)
