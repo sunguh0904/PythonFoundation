f990f4b71240c7fa08a127ad055b0c0be47cefd4
sunguh0904 <sunguh0904@gmail.com>
2024-08-09 20:22:51 +0900

    9-5 상속

---
+++ practice9-5.py
@@ -0,0 +1,28 @@
+# 상속
+class Unit:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+
+class AttackUnit(Unit):
+        def __init__(self, name, hp, damage):
+            Unit.__init__(self, name, hp)
+            self.damage = damage
+
+        def attack(self, loccation):
+             print(f"{self.name}: {loccation} 방향으로 적군을 공격 합니다. [공격력 {self.damage}]")
+
+        def damaged(self, damage):
+             print(f"{self.name}: {damage} 데미지를 입었습니다.")
+             self.hp -= damage
+             print(f"{self.name}: 현재 체력은 {self.hp}")
+             if self.hp <= 0:
+                  print(f"{self.name}: 파괴되었습니다.")
+
+# 파이어뱃: 공격 유닛, 화염방사기
+firebat1 = AttackUnit("파이어뱃", 50, 15)
+firebat1.attack("5시")
+
+# 공격 2번 받는다고 가정
+firebat1.damaged(25)
+firebat1.damaged(25)
