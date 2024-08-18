62213137f1a7acb1debc76388e4a7c4b3bcdd2f3
sunguh0904 <sunguh0904@gmail.com>
2024-08-18 19:14:48 +0900

    10-4 finally

---
+++ practice10-4.py
@@ -0,0 +1,22 @@
+# finally
+class BigNumberError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
+
+try:
+    print("한 자리 숫자 나누기 전용 계산기 입니다.")
+    num1 = int(input("첫 번째 숫자를 입력 하세요: "))
+    num2 = int(input("두 번째 숫자를 입력 하세요: "))
+    if num1 >= 10 or num2 >= 10:
+        raise BigNumberError(f"입력값: {num1}, {num2}")
+    print(f"{num1} / {num2} = {int(num1 / num2)}")
+except ValueError:
+    print("잘못된 값을 입력 하였습니다. 한 자리 숫자만 입력 하세요.")
+except BigNumberError as err:
+    print("에러가 발생 하였습니다. 한 자리 숫자만 입력 하세요.")
+    print(err)
+finally:
+    print("계산기를 이용해 주셔서 감사합니다.")
